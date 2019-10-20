5c9779142e1854bf8bb9829384bc11d9cfecf0e7
m7Aei <mateuspm2015@hotmail.com>
2019-10-20 20:30:19 -0300

    Criando o estilo do avatar e atualizando outros

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -4,7 +4,6 @@ import { darken } from 'polished';
 export const Container = styled.div`
   max-width: 600px;
   margin: 50px auto;
-
   form {
     display: flex;
     flex-direction: column;
@@ -25,7 +24,7 @@ export const Container = styled.div`
     }
 
     span {
-      color: #a52a2a;
+      color: #fb6f91;
       align-self: flex-start;
       margin: 0 0 10px;
       font-weight: bold;
@@ -42,16 +41,15 @@ export const Container = styled.div`
       margin: 5px 0 0;
       height: 44px;
       background: #3b9eff;
+      font-weight: bold;
+      color: #fff;
       border: 0;
       border-radius: 4px;
-      height: 44px;
-      padding: 0 15px;
-      color: #fff;
-      margin: 0 0 10px;
+      font-size: 16px;
       transition: background 0.2s;
 
       &:hover {
-        background: ${darken(0.08, '#3b9eff')};
+        background: ${darken(0.03, '#3b9eff')};
       }
     }
   }
@@ -61,16 +59,14 @@ export const Container = styled.div`
     margin: 10px 0 0;
     height: 44px;
     background: #f64c75;
+    font-weight: bold;
+    color: #fff;
     border: 0;
     border-radius: 4px;
-    height: 44px;
-    padding: 0 15px;
-    color: #fff;
-    margin: 0 0 10px;
+    font-size: 16px;
     transition: background 0.2s;
-
     &:hover {
-      background: ${darken(0.09, '#f64c75')};
+      background: ${darken(0.08, '#F64C75')};
     }
   }
 `;
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -13,12 +13,10 @@ export const Content = styled.div`
   width: 100%;
   max-width: 315px;
   text-align: center;
-
   form {
     display: flex;
     flex-direction: column;
     margin-top: 30px;
-
     input {
       background: rgba(0, 0, 0, 0.1);
       border: 0;
@@ -27,42 +25,35 @@ export const Content = styled.div`
       padding: 0 15px;
       color: #fff;
       margin: 0 0 10px;
-
       &::placeholder {
         color: rgba(255, 255, 255, 0.7);
       }
     }
-
     span {
-      color: #a52a2a;
+      color: #fb6f91;
       align-self: flex-start;
       margin: 0 0 10px;
       font-weight: bold;
     }
-
     button {
       margin: 5px 0 0;
       height: 44px;
       background: #3b9eff;
+      font-weight: bold;
+      color: #fff;
       border: 0;
       border-radius: 4px;
-      height: 44px;
-      padding: 0 15px;
-      color: #fff;
-      margin: 0 0 10px;
+      font-size: 16px;
       transition: background 0.2s;
-
       &:hover {
         background: ${darken(0.03, '#3b9eff')};
       }
     }
-
     a {
       color: #fff;
       margin-top: 15px;
       font-size: 16px;
       opacity: 0.8;
-
       &:hover {
         opacity: 1;
       }
