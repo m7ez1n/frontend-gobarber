fa209f7457659681aab295bc0b644b3322beed79
m7Aei <mateuspm2015@hotmail.com>
2019-10-20 19:42:31 -0300

    Criando e estilizando o formulário do usuario

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Dashboard() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
   return (
-    <div />
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Endereço de email" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Senha atual" />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme sua senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
   );
 }
