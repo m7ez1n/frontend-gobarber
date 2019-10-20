32877cd6fbb78780be687899142f33b13336a070
m7Aei <mateuspm2015@hotmail.com>
2019-10-20 17:47:56 -0300

    Configurando o Header com a autenticação com Token

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import api from '~/services/api';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>
+  api.get('appointments');
+  return <h1>Dashboard</h1>;
 }
